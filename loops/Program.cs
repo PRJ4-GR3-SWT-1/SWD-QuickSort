f1fef15b54a81161afa79323da6bc233f0428a00
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-22 11:02:47 +0200

    loop start

---
+++ loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch(); 
+            const long N = 40000000; 
+            double[] A, B, C; A = new double[N]; 
+            B = new double[N]; C = new double[N]; 
+            Random rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = rand.Next(); 
+                B[i] = rand.Next(); 
+                C[i] = rand.Next();
+            } 
+            Console.WriteLine("Starts sequential for now."); 
+            stopwatch.Start();
+            for (int i = 0; i < N; i++)
+            {
+                C[i] = A[i] * B[i];
+            } 
+            stopwatch.Stop(); Console.WriteLine("Sequential loop time in milliseconds: {0}", stopwatch.ElapsedMilliseconds); 
+            stopwatch.Reset(); Console.WriteLine("Finished");
+        }
+    }
+}
